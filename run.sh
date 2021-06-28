ffe9ca2179d2868d1bc09f540c2603e9edb4ba77
Krzysztof Klimczyk <kklimczyk@atlassian.com>
2021-06-29 09:24:40 +1000

    ccls updateds

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 echo "Compiling..."
-if (gcc -Wall -I./include -o clox *.c) then
+if (clang -MJ data.json -Wall -I./include -o clox *.c) then
     echo "Running clox"
     ./clox test_lox.lox
 fi
+sed -e '1s/^/[\'$'\n''/' -e '$s/,$/\'$'\n'']/' data.json > compile_commands.json
+rm data.json
